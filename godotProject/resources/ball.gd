ea3cf2f9a69223c3b8d2f2456d208b3b937e389b
Manisini <manisini.chakraborty@gmail.com>
2024-08-21 23:32:56 +0530

    double jump added

---
+++ godotProject/resources/ball.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name Ball
+
+
+@export var name : String = ""
+@export var jumpVelocity : float 
+@export var weight : float
+@export var horizontalSpeed : float
+@export var resilience : float
+@export var maxJumps : int
---
+++ godotProject/scripts/player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+@export var ball_mode : Ball
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var jump_count = 0
+var is_double_jump_collected : bool = false
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") :
+		if is_on_floor() && jump_count > 0 :
+			jump_count = 0
+		
+		if jump_count < ball_mode.maxJumps :
+			velocity.y = ball_mode.jumpVelocity
+			jump_count+= 1
+		
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+func enableDoubleJump() :
+	is_double_jump_collected = true
