91f860f17960c1201568690ba9419cebdfd77ca9
Manisini <manisini.chakraborty@gmail.com>
2024-09-17 19:03:13 +0530

    Added some dummy parallax for video recording

--- godotProject/scripts/interactables/swing.gd
+++ godotProject/scripts/interactables/swing.gd
@@ -3,13 +3,18 @@ extends Node2D
 @onready var ball_position: Marker2D = $swing/BallPosition
 var isPlayerOnSwing: bool = false
 var player: Player = null
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var timer: Timer = $Timer
+
+@export var timerDuration: float = 0.0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	timer.wait_time = timerDuration
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta: 																														float) -> void:
 	
 	if isPlayerOnSwing == true:
 		player.global_position = ball_position.global_position
@@ -34,3 +39,7 @@ func anyMovementDone() :
 func _on_ball_seat_body_entered(body: Node2D) -> void:
 	if body is Player:
 		isPlayerOnSwing = true
+
+
+func _on_timer_timeout() -> void:
+	animation_player.play("swing")
